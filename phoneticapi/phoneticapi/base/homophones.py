4ac85478f13c9aadc4161485e15ccb8f5ad82576
Edgar Zanella Alvarenga <e@vaz.io>
2023-08-26 16:25:26 +0200

    Use abc abd abstract class for base HomophonesDetector class

--- phoneticapi/phoneticapi/base/homophones.py
+++ phoneticapi/phoneticapi/base/homophones.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-class HomophonesDetector(object):
-    language = 'Language Name (e.g. German)'
-    method = 'Method Name (e.g. Colgne Phonetics)'
-    reference = 'An reference for the method, an URL, a preprint, etc'
+from abc import ABCMeta, abstractmethod, abstractproperty
 
+class HomophonesDetector:
+    __metaclass__ = ABCMeta
+
+    # Language Name (e.g. German)'
+    @abstractproperty
+    def language(self):
+        pass
+
+    # 'Method Name (e.g. Colgne Phonetics)'
+    @abstractproperty
+    def method(self):
+        pass
+
+    # 'An reference for the method, an URL, a preprint, etc'
+    @abstractproperty
+    def reference(self):
+        pass
+
+    @abstractmethod
     def compare(word1: str, word2: str) -> bool:
         pass
