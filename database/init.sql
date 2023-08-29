7c5416a948edca932dd5ba12858944251208ecdf
Edgar Zanella Alvarenga <e@vaz.io>
2023-08-29 11:05:14 +0200

    Simplifies database creation script

--- database/init.sql
+++ database/init.sql
@@ -1,11 +1,3 @@
-\set db_user `echo $DB_USER`
-\set db_pass `echo $DB_PASS`
-
-drop role if exists :db_user;
-create role :db_user with login password :'db_pass';
-
-create database phonetic;
-alter database phonetic owner to :db_user;
 create table homophones (word1 text, word2 text, lang text, pair_count integer default 1);
 create index homophones_idx on homophones (word1, word2, lang);
 
